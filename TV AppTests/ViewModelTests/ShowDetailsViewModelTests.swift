6220ded0cd9e315bd213e8499cc35cdf543adf8d
Rodolfo Roca <rroca1982@gmail.com>
2020-09-27 22:24:21 -0400

    - Test fixes - Updated readme

--- TV AppTests/ViewModelTests/ShowDetailsViewModelTests.swift
+++ TV AppTests/ViewModelTests/ShowDetailsViewModelTests.swift
@@ -60,7 +60,7 @@ class ShowDetailsViewModelTests: XCTestCase {
             XCTAssertEqual(viewModel.time, "22:00")
             XCTAssertEqual(viewModel.days, "Thursday")
             
-            XCTAssertEqual(viewModel.summary?.string, "Under the Dome is the story of a small town that is suddenly and inexplicably sealed off from the rest of the world by an enormous transparent dome. The town\'s inhabitants must deal with surviving the post-apocalyptic conditions while searching for answers about the dome, where it came from and if and when it will go away.")
+            XCTAssertEqual(viewModel.summary, "<p><b>Under the Dome</b> is the story of a small town that is suddenly and inexplicably sealed off from the rest of the world by an enormous transparent dome. The town\'s inhabitants must deal with surviving the post-apocalyptic conditions while searching for answers about the dome, where it came from and if and when it will go away.</p>")
         } else {
             XCTFail("Model is nil")
         }
